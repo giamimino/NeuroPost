1085d0dcaa32e2133cf0a9f970ca8b32a3a7b63d
giamimino <miminoshvili2102@example.com>
2026-03-19 22:03:11 +0400

    chore: add responsive to foryou page and comments sections

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -11,11 +11,14 @@ export default function MainLayout({
 }) {
   const { comment } = useCommentsStore();
 
+  const responsive =
+    window.innerWidth < 640 ? "100%" : window.innerWidth < 768 ? "45%" : "66%";
+
   return (
     <div className="flex">
       <motion.div
         className="min-w-0"
-        animate={{ flexBasis: comment ? "66%" : "100%" }}
+        animate={{ flexBasis: comment ? responsive : "100%" }}
         transition={{ type: "spring", stiffness: 120, damping: 20 }}
       >
         {children}
