44e72226927a1e792ef02ac053a0339bc44a563c
giamimino <miminoshvili2102@example.com>
2026-03-23 20:32:21 +0400

    fix format using prettier

--- src/app/tags/[tag]/page.tsx
+++ src/app/tags/[tag]/page.tsx
@@ -47,12 +47,7 @@ const TagPage = ({ params }: { params: Promise<{ tag: string }> }) => {
         <div>
           <CardTitle className="text-xl">All #{tag} posts</CardTitle>
         </div>
-        {loading && (
-          <SkeletonPosts
-            length={5}
-            className="w-1/3"
-          />
-        )}
+        {loading && <SkeletonPosts length={5} className="w-1/3" />}
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-5 w-full px-10 ">
           {posts.map((post) => (
             <Card
