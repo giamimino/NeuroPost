9df2a831f8e8524c32c7baa0af70f96c4c854832
giamimino <miminoshvili2102@example.com>
2026-03-23 18:49:30 +0400

    add unread notifications clean up and schedule changed from 10PM > 12PM, add monitoring

--- src/app/api/cron-jobs/notifications-cleanup/route.ts
+++ src/app/api/cron-jobs/notifications-cleanup/route.ts
@@ -10,12 +10,25 @@ export async function POST(req: Request) {
     }
 
     await sql.query(
-      `DELETE FROM notifications WHERE NOW() > created_at + INTERVAL '30 days' AND isread = true`,
+      `DELETE FROM notifications WHERE 
+        (NOW() > created_at + INTERVAL '30 days' AND isread = true) OR 
+        (NOW() > created_at + INTERVAL '60 days' AND isread = false)`,
     );
+
+    await sql.query(
+      `INSERT INTO cron_logs (job_name, status) VALUES ($1, $2)`,
+      ["cleanup_notifications", "success"]
+    )
 
     return NextResponse.json({ ok: true }, { status: 200 });
   } catch (err) {
     console.error(err);
+
+    await sql.query(
+      `INSERT INTO cron_logs (job_name, status, error) VALUES ($1, $2, $3)`,
+      ["cleanup_notifications", "failed", JSON.stringify(err)]
+    )
+
     return NextResponse.json({ ok: false, message: "" }, { status: 500 });
   }
 }
