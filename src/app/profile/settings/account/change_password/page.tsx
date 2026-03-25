7a5e6f77b81e2029e957d63ba97dcc9fbfbd3586
giamimino <miminoshvili2102@example.com>
2026-03-25 22:10:44 +0400

    chore(change_password): add change password front end handler, change password development done

--- src/app/profile/settings/account/change_password/page.tsx
+++ src/app/profile/settings/account/change_password/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { CardTitle } from "@/components/ui/card";
+import { CardDescription, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Spinner } from "@/components/ui/spinner";
+import { ApiConfig } from "@/configs/api-configs";
+import { ERRORS } from "@/constants/error-handling";
+import { apiFetch } from "@/lib/apiFetch";
+import { useAlertStore } from "@/store/zustand/alertStore";
 import { ChevronLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
@@ -10,9 +15,38 @@ const ChangePasswordSettingPage = () => {
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [isChanged, setIsChanged] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const { addAlert } = useAlertStore();
   const router = useRouter();
 
-  const handleSave = () => {};
+  const handleSave = async () => {
+    try {
+      setLoading(true);
+      const res = await apiFetch("/api/user/change_password", {
+        ...ApiConfig.post,
+        body: JSON.stringify({ oldPassword, newPassword }),
+      });
+      const data = await res?.json();
+
+      if (data.error)
+        addAlert({ id: crypto.randomUUID(), type: "error", ...data.error });
+      if (data.ok) {
+        setIsSuccess(true);
+        setOldPassword("");
+        setNewPassword("");
+        setIsChanged(false);
+      }
+    } catch {
+      addAlert({
+        id: crypto.randomUUID(),
+        type: "error",
+        ...ERRORS.GENERIC_ERROR,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const handleCancel = () => {
     setOldPassword("");
@@ -33,6 +67,14 @@ const ChangePasswordSettingPage = () => {
     };
     onChange();
   }, [oldPassword, newPassword]);
+
+  useEffect(() => {
+    if (!isSuccess) return;
+
+    const timer = setTimeout(() => setIsSuccess(false), 2000);
+
+    return () => clearTimeout(timer);
+  }, [isSuccess]);
 
   return (
     <div className="flex flex-col gap-5">
@@ -62,21 +104,26 @@ const ChangePasswordSettingPage = () => {
             onChange={(e) => setNewPassword(e.target.value)}
           />
         </div>
-        <div className="flex gap-3 font-plusJakartaSans justify-end">
-          <Button
-            onClick={handleCancel}
-            variant={"secondary"}
-            className="cursor-pointer"
-          >
-            Cancel
-          </Button>
-          <Button
-            variant={!isChanged ? "disabled" : "destructive"}
-            className={`${isChanged ? "cursor-pointer" : ""}`}
-            onClick={isChanged ? handleSave : undefined}
-          >
-            Save
-          </Button>
+        <div className="flex justify-end">
+          <div className="flex flex-col items-center gap-3">
+            <div className="flex gap-3 font-plusJakartaSans">
+              <Button
+                onClick={handleCancel}
+                variant={"secondary"}
+                className="cursor-pointer"
+              >
+                Cancel
+              </Button>
+              <Button
+                variant={!isChanged ? "disabled" : "destructive"}
+                className={`${isChanged ? "cursor-pointer" : ""}`}
+                onClick={isChanged && !loading ? handleSave : undefined}
+              >
+                {loading ? <Spinner /> : <span>Save</span>}
+              </Button>
+            </div>
+            {isSuccess && <CardDescription>Password changed!</CardDescription>}
+          </div>
         </div>
       </div>
     </div>
