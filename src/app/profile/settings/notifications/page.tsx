cf04ce322f147073e28d36f7a0723853bfc87e64
giamimino <miminoshvili2102@example.com>
2026-03-30 12:53:33 +0400

    chore(notifications_settings): add settings fetch

--- src/app/profile/settings/notifications/page.tsx
+++ src/app/profile/settings/notifications/page.tsx
@@ -1,37 +1,40 @@
 "use client";
 import { ToggleSwitch } from "@/components/common/toggle";
 import { CardDescription, CardTitle } from "@/components/ui/card";
-import React, { useState } from "react";
+import { ApiConfig } from "@/configs/api-configs";
+import { apiFetch } from "@/lib/apiFetch";
+import { useAlertStore } from "@/store/zustand/alertStore";
+import React, { useEffect, useState } from "react";
 
-const $settings = [
-  {
+const $settings = {
+  LIKES: {
     id: "likes",
     label: "Likes",
     description: "Receive notifications when someone likes your post.",
   },
-  {
+  COMMENTS: {
     id: "comments",
     label: "Comments",
     description: "Receive notifications when someone comments on your post.",
   },
-  {
+  NEW_FOLLOWERS: {
     id: "new_followers",
     label: "New Followers",
     description: "Receive notifications when someone starts following you.",
   },
-  {
+  FRIEND_REQUESTS: {
     id: "friend_requests",
     label: "Friend Requests",
     description:
       "Receive notifications when someone sends you a friend request.",
   },
-  {
+  FRIEND_ACCEPTS: {
     id: "friend_accepts",
     label: "Friend Accepts",
     description:
       "Receive notifications when someone accepts your friend request.",
   },
-];
+};
 
 const SettingsNotificationsPage = () => {
   const [settings, setSettings] = useState({
@@ -41,12 +44,29 @@ const SettingsNotificationsPage = () => {
     friend_requests: false,
     friend_accepts: false,
   });
+  const { addAlert } = useAlertStore();
+
+  useEffect(() => {
+    const url = `/api/user/settings?category=NOTIFICATIONS_SETTINGS_KEYS`;
+    apiFetch(url, ApiConfig.get)
+      .then((res) => res?.json())
+      .then((data) => {
+        if (data.ok) {
+          console.log(data);
+        } else if (data.error) {
+          addAlert({ id: crypto.randomUUID(), type: "error", ...data.error });
+        }
+      });
+  }, []);
 
   return (
-    <div className="flex flex-col gap-6 w-full">
+    <div
+      data-lenis-prevent
+      className="flex flex-col gap-6 w-full overflow-auto"
+    >
       <CardTitle>Notifications</CardTitle>
       <div className="flex flex-col gap-2 w-full">
-        {$settings.map((setting) => (
+        {Object.values($settings).map((setting) => (
           <div
             key={setting.id}
             className="cursor-pointer flex text-muted-foreground justify-between w-full items-center hover:text-foreground group hover:bg-accent p-2 rounded-md"
