e26e1f76349f944fa59929c7b20444f1d356572a
giamimino <miminoshvili2102@example.com>
2026-04-02 23:55:52 +0400

    chore: move rate limit from middleware to function, add api rules each API have own limit

---
+++ client/src/middlewares/rateLimit.middleware.ts
@@ -0,0 +1,67 @@
+import { ERRORS } from "@/constants/error-handling";
+import { NextRequest, NextResponse } from "next/server";
+
+const ipRequestMap = new Map<string, number[]>();
+
+const rules = [
+  { match: "/^\/api\/auth/", limit: 3 },
+  { match: "/^\/api\/follow", limit: 3 },
+  { match: "/^\/api\/friend-request", limit: 3 },
+  { match: "/^\/api\/friend-request", limit: 3 },
+  { match: "/^\/api\/friends", limit: 7 },
+  { match: "/^\/api\/index", limit: 30 },
+  { match: "/^\/api\/notifications", limit: 30 },
+  { match: "/^\/api\/post", limit: 10 },
+  { match: "/^\/api\/r2", limit: 20 },
+  { match: "/^\/api\/redis", limit: 5 },
+  { match: "/^\/api\/redis", limit: 5 },
+  { match: "/^\/api\/search", limit: 13 },
+  { match: "/^\/api\/send", limit: 2 },
+  { match: "/^\/api\/tags", limit: 10 },
+  { match: "/^\/api\/user", limit: 7 },
+];
+
+const RATE_LIMIT_WINDOW = 60 * 1000;
+const MAX_REQ = 5;
+
+const getRule = (path: string) => {
+  return rules.find((r) => r.match.startsWith(path));
+};
+
+export default function RateLimitMiddleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const rule = getRule(path);
+
+  if (!rule)
+    return NextResponse.json(
+      { ok: false, error: ERRORS.GENERIC_ERROR },
+      { status: 400 },
+    );
+
+  const ip =
+    (req.headers.get("x-forwarded-for") ?? "").split(",")[0].trim() ||
+    // @ts-ignore
+    (typeof req.ip === "string" ? req.ip : "") ||
+    "unknown";
+  const key = `${ip}:${rule.match}`;
+
+  if (!ipRequestMap.has(key)) {
+    ipRequestMap.set(key, []);
+  }
+
+  const currentTime = Date.now();
+
+  const timestamps = ipRequestMap
+    .get(key)!
+    .filter((ts) => currentTime - ts < RATE_LIMIT_WINDOW);
+
+  if (timestamps.length >= rule.limit) {
+    return NextResponse.json(
+      { error: ERRORS.TOO_MANY_REQUESTS },
+      { status: 429 },
+    );
+  }
+
+  timestamps.push(currentTime);
+  ipRequestMap.set(key, timestamps);
+}
--- client/src/proxy.ts
+++ client/src/proxy.ts
@@ -1,41 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import AuthMiddleware from "./middlewares/auth.middleware";
-import { ERRORS } from "./constants/error-handling";
-
-const ipRequestMap = new Map<string, number[]>();
-
-const RATE_LIMIT_WINDOW = 60 * 1000;
-const MAX_REQ = 5;
+import RateLimitMiddleware from "./middlewares/rateLimit.middleware";
 
 export default async function proxy(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
 
   if (pathname.startsWith("/api")) {
-    const ip =
-      (req.headers.get("x-forwarded-for") ?? "").split(",")[0].trim() ||
-      // @ts-ignore
-      (typeof req.ip === "string" ? req.ip : "") ||
-      "unknown";
-
-    if (!ipRequestMap.has(ip)) {
-      ipRequestMap.set(ip, []);
-    }
-
-    const currentTime = Date.now();
-
-    const timestamps = ipRequestMap
-      .get(ip)!
-      .filter((ts) => currentTime - ts < RATE_LIMIT_WINDOW);
-
-    if (timestamps.length >= MAX_REQ) {
-      return NextResponse.json(
-        { error: ERRORS.TOO_MANY_REQUESTS },
-        { status: 429 },
-      );
-    }
-
-    timestamps.push(currentTime);
-    ipRequestMap.set(ip, timestamps);
+    const rateLimitRes = RateLimitMiddleware(req)
+    return rateLimitRes
   }
 
   if (pathname.startsWith("/profile") && pathname.startsWith("/auth")) {
