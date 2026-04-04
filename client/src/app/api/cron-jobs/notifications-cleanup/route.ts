ccdaa0a049b3d46979b5d529fdfce0128b9022dd
giamimino <miminoshvili2102@example.com>
2026-04-04 23:19:05 +0400

    improve error handling, add logging temporary

--- client/src/app/api/cron-jobs/notifications-cleanup/route.ts
+++ client/src/app/api/cron-jobs/notifications-cleanup/route.ts
@@ -1,12 +1,16 @@
+import { ERRORS } from "@/constants/error-handling";
 import { sql } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     if (
-      req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+      req.headers.get("authorization") !== `Bearer ${process.env.CRON_SECRET}`
     ) {
-      return NextResponse.json("Unauthorized", { status: 401 });
+      return NextResponse.json(
+        { ok: false, error: ERRORS.UNAUTHORIZED },
+        { status: 401 },
+      );
     }
 
     await sql.query(
@@ -24,11 +28,30 @@ export async function POST(req: Request) {
   } catch (err) {
     console.error(err);
 
-    await sql.query(
-      `INSERT INTO cron_logs (job_name, status, error) VALUES ($1, $2, $3)`,
-      ["cleanup_notifications", "failed", JSON.stringify(err)],
-    );
+    try {
+      await sql.query(
+        `INSERT INTO cron_logs (job_name, status, error) VALUES ($1, $2, $3)`,
+        ["cleanup_notifications", "failed", JSON.stringify(err)],
+      );
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json(
+        {
+          ok: false,
+          error: ERRORS.INTERNAL_SERVER_ERROR,
+          dev: JSON.stringify(error),
+        },
+        { status: 500 },
+      );
+    }
 
-    return NextResponse.json({ ok: false, message: "" }, { status: 500 });
+    return NextResponse.json(
+      {
+        ok: false,
+        error: ERRORS.INTERNAL_SERVER_ERROR,
+        dev: JSON.stringify(err),
+      },
+      { status: 500 },
+    );
   }
 }
