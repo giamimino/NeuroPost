b47a7376d9c795305011cf78e8e20250fb8b1e72
giamimino <miminoshvili2102@example.com>
2026-04-19 00:40:04 +0400

    add password_reset check API route

---
+++ client/src/app/api/send/password_reset/check/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import crypto from "crypto";
+import client from "@/lib/client";
+import { ERRORS } from "@/constants/error-handling";
+import { cookies } from "next/headers";
+import { createPasswordResetToken } from "@/lib/jwt";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { token } = body;
+
+    const hash = crypto.createHash("sha256").update(token).digest("hex");
+
+    const key = `reset:${hash}`;
+
+    const user = await client.get(key);
+    if (!user)
+      return NextResponse.json(
+        { ok: false, error: ERRORS.VERIFICATION_TOKEN_INVALID },
+        { status: 404 },
+      );
+
+    const parsedUser = JSON.parse(user);
+
+    const cookieStore = await cookies();
+    const password_reset_token = createPasswordResetToken(parsedUser);
+
+    cookieStore.set(
+      process.env.PASSWORD_RESET_COOKIE_NAME!,
+      password_reset_token,
+      {
+        httpOnly: true,
+        maxAge: 60 * 15,
+        secure: true,
+        sameSite: "strict",
+        path: "/",
+      },
+    );
+
+    return NextResponse.json(
+      { ok: true, user: { username: parsedUser.username } },
+      { status: 200 },
+    );
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ ok: false, message: "" }, { status: 500 });
+  }
+}
