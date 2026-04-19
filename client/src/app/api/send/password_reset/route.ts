36912b4f7247c5446fd3873adfb0b0cdad5fdc53
giamimino <miminoshvili2102@example.com>
2026-04-19 15:55:07 +0400

    feat(password-reset): add password reset completion endpoint

--- client/src/app/api/send/password_reset/route.ts
+++ client/src/app/api/send/password_reset/route.ts
@@ -1,11 +1,17 @@
 import { ERRORS } from "@/constants/error-handling";
 import { sql } from "@/lib/db";
-import { EmailInObjectSchema } from "@/schemas/auth/auth.schema";
+import {
+  EmailInObjectSchema,
+  PasswordValidatorSchema,
+} from "@/schemas/auth/auth.schema";
 import { NextResponse } from "next/server";
 import crypto from "crypto";
 import client from "@/lib/client";
 import { Resend } from "resend";
 import { PasswordResetEmailTemplate } from "@/components/email-templates";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -58,14 +64,65 @@ export async function POST(req: Request) {
       react: PasswordResetEmailTemplate({ url }),
     });
 
-    if(error) {
+    if (error) {
       return NextResponse.json(
         { ok: false, error: ERRORS.EMAIL_VERIFICATION_SEND_ERROR },
-        { status: 500 }
-      )
+        { status: 500 },
+      );
     }
 
     return NextResponse.json({ ok: true, data }, { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ ok: false, message: "" }, { status: 500 });
+  }
+}
+
+export async function PUT(req: Request) {
+  try {
+    const body = await req.json();
+
+    const cookieStore = await cookies();
+    const password_reset_token = cookieStore.get(
+      process.env.PASSWORD_RESET_COOKIE_NAME!,
+    )?.value;
+
+    if (!password_reset_token)
+      return NextResponse.json(
+        { ok: false, error: ERRORS.PASSWORD_RESET_TOKEN_INVALID },
+        { status: 400 },
+      );
+
+    let payload;
+    try {
+      payload = jwt.verify(
+        password_reset_token,
+        process.env.PASSWORD_RESET_SECRET!,
+      ) as any;
+    } catch {
+      return NextResponse.json(
+        { ok: false, error: ERRORS.PASSWORD_RESET_TOKEN_EXPIRED },
+        { status: 400 },
+      );
+    }
+
+    const parsedPassword = PasswordValidatorSchema.safeParse(body.password);
+
+    if (!parsedPassword.success) {
+      const message = JSON.parse(parsedPassword.error.issues[0].message);
+
+      return NextResponse.json({ ok: false, error: message }, { status: 400 });
+    }
+
+    const password = parsedPassword.data;
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    await sql.query(
+      `UPDATE users SET password = $1 WHERE id = $2`,
+      [hashedPassword, payload.userId]
+    )
+
+    return NextResponse.json({ ok: true }, { status: 200 });
   } catch (err) {
     console.error(err);
     return NextResponse.json({ ok: false, message: "" }, { status: 500 });
