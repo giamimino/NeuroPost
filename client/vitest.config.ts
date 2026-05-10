beb59ddf1e806b8609d3f38dc0746d443eac88d5
giamimino <miminoshvili2102@example.com>
2026-05-10 21:30:45 +0400

    add testing setup with jsdom and react testing library

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
-    include: ['src/tests/*.test.ts'],
+    environment: 'jsdom',
+    include: ['src/tests/**/*.test.ts', 'src/tests/**/*.test.tsx'],
+    setupFiles: "./src/tests/setup.ts"
   },
 })
