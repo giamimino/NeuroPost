153e34bc934dc121a586e9a2f814311744b7a931
giamimino <miminoshvili2102@example.com>
2026-05-10 21:03:21 +0400

    feat: add indexPost.ts and indexSearchWordNormalize

--- client/src/utils/indexPost.ts
+++ client/src/utils/indexPost.ts
@@ -1,6 +1,24 @@
+import { indexSearchWordNormalize } from "./functions/indexSearchWordNormalize";
 
+export default function indexPost(post: {
+  id: number;
+  title: string;
+  description: string;
+}) {
+  const fields = ["title", "description"] as const;
 
+  const indexedWords = new Map<
+    string,
+    { title?: boolean; description?: boolean }
+  >();
 
-export default function indexPost(postId: number) {
-  
+  for (const field of fields) {
+    const normalizedWords = indexSearchWordNormalize(post[field]).split(" ");
+
+    for (const word of normalizedWords) {
+      indexedWords.set(word, { ...indexedWords.get(word), [field]: true });
+    }
+  }
+
+  return indexedWords
 }
