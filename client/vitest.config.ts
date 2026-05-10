17ae7db032ab3e73eb689b56498c9ec62d1129d8
giamimino <miminoshvili2102@example.com>
2026-05-10 22:19:45 +0400

    fix(client/vitest.config): configure imports with '@/'

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,10 +1,16 @@
+import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     include: ['src/tests/**/*.test.ts', 'src/tests/**/*.test.tsx'],
-    setupFiles: "./src/tests/setup.ts"
+    setupFiles: "./src/tests/setup.ts",
   },
 })
